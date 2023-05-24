450cb8ecac12c83b278093747e20ac68e97cc44e
Bulela Gomoshe <bulelagomoshe1@gmail.com>
2023-05-24 15:00:05 +0200

    server b4 tour of heroes , its running

---
+++ controller/user.controller.js
@@ -0,0 +1,107 @@
+const db = require("../models")
+const User = db.All
+
+// GET ALL USERS 
+exports.getAll = (req, res)=>{
+
+
+       User.find()
+           .then(data=>{
+               res.send(data)
+               console.log(data)
+           })
+           .catch(error=>{
+            res.status(500).send("Could not find user", error)
+            console.log("Could not find user", error)
+           })
+}
+
+
+// CREATE 1 USER
+exports.create = async (req, res)=>{
+    if(!req.body){
+            res.status(400).send("Cannot add without info")
+            return;   
+    }
+    let results;
+
+    await fetch('https://randomuser.me/api/')
+            .then(res=>res.json())
+            .then(data=>{
+            console.log(results=data.results[0].email)})
+
+    const user = new User ({
+           fname: req.body.fname,
+           email: results,
+           password: req.body.password
+    })
+   
+    try{
+        user.save()
+        .then(user=>{
+
+            console.log(user)
+            res.send(user)  
+        })
+        return
+    }catch (err){
+        res.status(500).send('Could not create new user')
+        console.log(`Some err occured : ${err.message}`)
+    }
+}     
+
+
+
+// CLEAR ALL
+exports.deleteAll = (req, res)=>{
+
+    User.deleteMany()
+        .then(data=>{
+            res.send(data)
+            console.log(data)
+        })
+        .catch(error=>{
+            res.status(500).send("Could not delete all users ", error)
+            console.log("Could not delete all", error)
+        })
+}
+
+
+// DELETE A USER
+exports.deleteOne = (req, res)=>{
+    
+    const id = req.params.id
+
+    User.findByIdAndRemove(id, { useFindAndModify: false})
+        .then(data =>{
+            if(!data) {
+               res.status(404).send({
+                msg: `Cannot delete User with id=${id}. Maybe it was not exit/existing`
+               })
+            } else res.status(201).send({ msg: "User was deleted successfully"})
+        })
+        .catch(err => {
+            res.status(500).send({ msg: `Error deleting User with id=${id}, Error:  ${err}`})
+        })
+}
+
+// UPDATE A USER
+exports.update = (req, res)=>{
+    if(!req.body){
+        res.status(400).send("Cannot update user")
+        return
+    }
+    const id = req.params.id
+
+    User.findByIdAndUpdate(id, req.body, { useFindAndModify: false})
+    .then(data =>{
+        if(!data) {
+           res.status(404).send({
+             msg: `Cannot update User with id=${id}. Maybe it was not found`
+           })
+        } else res.status(201).send({ msg: "User was updated successfully"})
+    })
+    .catch(err => {
+        res.status(500).send({ msg: `Error updating User with id=${id} ${err}`})
+    })
+}
---
+++ routes/user.routes.js
@@ -0,0 +1,17 @@
+module.exports = rs => {
+    const router = require("express").Router();
+    const controller = require("../controller/user.controller")
+
+
+    router.post('/signup', controller.create); // ADD USER
+
+    router.get('/', controller.getAll); // GET ALL USERS
+
+    router.delete('/', controller.deleteAll); // DELETE ALL USERS
+
+    router.put('/:id', controller.update)// UPDATE 1 USER
+
+    router.delete('/:id', controller.deleteOne); // DELETE 1 USERS
+
+rs.use('/v1/users',router)  
+}
